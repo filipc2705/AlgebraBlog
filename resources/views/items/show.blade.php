b02276d4bbf6278d6151e37f9f5dd1a00d4c1ef7
programeri <programeri@ucione.local>
2017-05-05 19:37:23 +0200

    Dodavanje datuma i vremena

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row">
 			<div class="col-md-8 col-md-offset-2">
 					<h2>{{ $item->title }}</h2>
 					<div>{{ $item->content }}</div>
+					<div>{{ $item->created_at }}</div>
 				</div>
 			</div>
 		</div>
